5906528b7080946fb09b813b18525368a485784b
Qquanwei <quanwei9958@126.com>
2025-06-29 15:42:29 +0800

    feat: erb

--- .erb/scripts/ElectronRebuild.js
+++ .erb/scripts/ElectronRebuild.js
@@ -1,22 +1,22 @@
-import path from 'path';
-import { execSync } from 'child_process';
-import fs from 'fs';
-import { dependencies } from '../../src/package.json';
+import path from "path";
+import { execSync } from "child_process";
+import fs from "fs";
+import { dependencies } from "../../src/package.json";
 
-const nodeModulesPath = path.join(__dirname, '../../src/node_modules');
+const nodeModulesPath = path.join(__dirname, "../../src/node_modules");
 
 if (
   Object.keys(dependencies || {}).length > 0 &&
   fs.existsSync(nodeModulesPath)
 ) {
   const electronRebuildCmd =
-    '../node_modules/.bin/electron-rebuild --parallel --force --types prod,dev,optional --module-dir .';
+    "../node_modules/.bin/electron-rebuild --parallel -v 33.2.1 --force --types prod,dev,optional --module-dir .";
   const cmd =
-    process.platform === 'win32'
-      ? electronRebuildCmd.replace(/\//g, '\\')
+    process.platform === "win32"
+      ? electronRebuildCmd.replace(/\//g, "\\")
       : electronRebuildCmd;
   execSync(cmd, {
-    cwd: path.join(__dirname, '../../src'),
-    stdio: 'inherit',
+    cwd: path.join(__dirname, "../../src"),
+    stdio: "inherit",
   });
 }
